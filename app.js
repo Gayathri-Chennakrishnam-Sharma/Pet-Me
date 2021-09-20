710a155e32fa06b04b9d8c77ba8702b784fc9f23
Gayathri-Chennakrishnam-au16 <sharmaanaga620@gmail.com>
2021-09-20 11:42:24 +0530

    Updated Basic routing.

--- app.js
+++ app.js
@@ -2,23 +2,12 @@ const fs = require('fs');
 const express = require('express');
 
 const app = express();
-
 app.use(express.json());
-
-//routes-sample code:
-// app.get('/', (req, res) => {
-//     res.status(200)
-//        .json({message:'Hello from the server side 🌻 🌻', app: 'Pet me'});
-// });
-
-// app.post('/', (req,res) => {
-//     res.send('You can post to this api endpoint!...');
-// });
 
 const pets = JSON.parse(fs.readFileSync(`${__dirname}/dev-data/data/pets-simple.json`));
 
-//routes-actual routes:
-app.get('/api/v1/pets', (req, res) => {
+//route-handlers:
+const getAllPets = (req, res) => {
     res.status(200).json({
         status: 'Success',
         results: pets.length,
@@ -26,20 +15,31 @@ app.get('/api/v1/pets', (req, res) => {
             pets
         }
     })
-});
+};
 
-app.get('/api/v1/pets/:id', (req, res) => {
+const getAPet = (req, res) => {
     console.log(req.params);
+
+    const id = req.params.id * 1;
+    const pet = pets.find(el => el.id === id);
+
+    if((!pet || id > pets.length)){
+        return res.status(404).json({
+            status: 'Fail',
+            message: 'Invalid ID'
+        })
+
+    }
+
     res.status(200).json({
         status: 'Success',
-        // results: pets.length,
-        // data:{
-        //     pets
-        // }
+        data:{
+            pet
+        }
     })
-});
+};
 
-app.post('/api/v1/pets', (req, res) => {
+const createAPet = (req, res) => {
     // console.log(req.body);
 
     const newId = pets[pets.length-1].id + 1;
@@ -55,7 +55,56 @@ app.post('/api/v1/pets', (req, res) => {
             }
         });
     });
-});
+};
+
+const updateApet =  (req,res) => {
+    if((req.params.id * 1 > pets.length)){
+        return res.status(404).json({
+            status: 'Fail',
+            message: 'Invalid ID'
+        });
+    }
+        res.status(200).json({
+            status: 'Success',
+            data:{
+                pet : '<Updated Pet here...>'
+            }
+
+        });
+};
+
+const deleteAPet =  (req,res) => {
+    if((req.params.id * 1 > pets.length)){
+        return res.status(404).json({
+            status: 'Fail',
+            message: 'Invalid ID'
+        });
+    }
+        res.status(204).json({
+            status: 'Success',
+            data: null
+
+        });
+} ;
+
+//routes:
+// app.get('/api/v1/pets', getAllPets );
+// app.get('/api/v1/pets/:id', getAPet );
+// app.post('/api/v1/pets', createAPet );
+// app.patch('/api/v1/pets/:id', updateApet);
+// app.delete('/api/v1/pets/:id', deleteAPet );
+
+
+//simplified routes:
+app.route('/api/v1/pets')
+.get(getAllPets)
+.post(createAPet);
+
+app.route('/api/v1/pets/:id')
+.get(getAPet)
+.patch(updateApet)
+.delete(deleteAPet);
+
 
 const port = 3001;
 app.listen(port, () => {
